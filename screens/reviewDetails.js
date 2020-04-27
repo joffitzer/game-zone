04f0dcb0cfb6d11871389a0d221f60d3eaab977c
joffitzer <joffitzer@gmail.com>
2020-04-27 11:30:16 -0400

    navigating between home screen and details screen using stack nav

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { globalStyles } from '../styles/global'
 
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
+
+    const pressHandler = () => {
+        // navigation.pop()
+        navigation.goBack();
+    }
+
     return (
         <View style={globalStyles.container}>
             <Text>Review Details Screen</Text>
+            <Button title='back to home' onPress={pressHandler}/>
         </View>
     )
 }
